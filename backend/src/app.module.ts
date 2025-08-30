e4bdd3d647f7fd20e2f8522f2a489a836b8e9bd5
Gleb Tsurakov <glebtsurakov@Glebs-MacBook-Pro.local>
2025-08-30 17:50:55 +0300

    Fix: remove broken Prisma imports for deployment

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,15 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-//import { PrismaService } from './prisma/prisma.service';
 import { ProjectModule } from './project/project.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { UploadModule } from './upload/upload.module';
 import { ProcessingService } from './processing/processing.service';
 
 @Module({
-  imports: [ProjectModule, /* PrismaModule, */ UploadModule],
+  imports: [ProjectModule, UploadModule],
   controllers: [AppController],
-  providers: [AppService, /* PrismaService, */ ProcessingService],
+  providers: [AppService, ProcessingService],
 })
 export class AppModule {}
