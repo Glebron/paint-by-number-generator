e419f127d0e1e24566297c6e25ce337ca566c4f7
Gleb Tsurakov <glebtsurakov@Glebs-MacBook-Pro.local>
2025-08-30 17:54:46 +0300

    Remove Prisma folder for MVP deployment

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProjectModule } from './project/project.module';
-import { PrismaModule } from './prisma/prisma.module';
 import { UploadModule } from './upload/upload.module';
 import { ProcessingService } from './processing/processing.service';
 
