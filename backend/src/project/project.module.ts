40b39d0f0df599e5bae6f05f800cd182a5f74fad
Gleb Tsurakov <glebtsurakov@Glebs-MacBook-Pro.local>
2025-08-30 17:58:32 +0300

    Disable ProjectService for deployment (remove Prisma)

--- backend/src/project/project.module.ts
+++ backend/src/project/project.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ProjectService } from './project.service';
 import { ProjectController } from './project.controller';
-import { PrismaService } from '../prisma/prisma.service';
 import { ProcessingService } from '../processing/processing.service'; // ADD THIS
 
 @Module({
   controllers: [ProjectController],
-  providers: [ProjectService, PrismaService, ProcessingService],
+  providers: [ProjectService, ProcessingService],
 })
 export class ProjectModule {}
